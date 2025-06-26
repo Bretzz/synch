0e8c59465a5c5f3341ebeb5405f2c93eca377e7b
Tommaso Piana Agostinetti <totommi@Tommasos-MBP.station>
2025-06-27 01:52:13 +0200

    better path

--- synch.sh
+++ synch.sh
@@ -4,6 +4,6 @@ ls ~/ | grep synch || (echo "fatal: no repo" && exit 1)
 
 ls ~/ | grep storage || (echo "fatal: no storage" && exit 1)
 
-cp ~/storage/savs/*.sav ~/synch
+cp ~/storage/shared/savs/*.sav ~/synch
 
 cd ~/synch; git pull && git add . && git commit -m "$(date)";
