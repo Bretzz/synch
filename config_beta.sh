73bcf443e710570c84a1b45f33d324a071b92588
Tommaso Piana Agostinetti <totommi@Tommasos-MBP.station>
2025-06-27 02:20:41 +0200

    naminngs 2

---
+++ config_beta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ls ~/.ssh | grep .pub || (ssh-keygen && exit)
+echo FOUND SSH
+
+ls | grep synch || git clone git@github.com:Bretzz/synch.git
+echo FOUND REPO
+
+ls | grep storage || termux-setup-storage
+echo FOUND STORAGE
+
+mv ~/synch/synch.sh ~/.shortcuts/
