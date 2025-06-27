7cb8baaf998308126bedf7dba6b962f07a477d21
Bretzz <fake@email.com>
2025-06-27 03:16:38 +0200

    Fri Jun 27 03:16:38 CEST 2025

--- synch_mau.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls ~/ | grep synch || (echo "fatal: no repo" && exit 1)
-
-ls ~/ | grep storage || (echo "fatal: no storage" && exit 1)
-
-cp ~/storage/shared/Uffa_butstillthnx/*.sav ~/synch
-
-cd ~/synch; git pull;  git add . && git commit -m "$(date)"; git push
