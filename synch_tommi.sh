196f3fd53a19960bea73a281f895eda5b91b2ad7
Tommaso Piana Agostinetti <totommi@Tommasos-MBP.station>
2025-06-27 03:15:36 +0200

    new_setup_bugfix

--- synch_tommi.sh
+++ synch_tommi.sh
@@ -4,6 +4,6 @@ ls ~/ | grep storage || (echo "fatal: no storage" && exit 1)
 
 ls ~/ | grep synch || (echo "fatal: no repo" && exit 1)
 
-cd ~/storage/synch; git pull
+cd ~/storage/shared/synch; git pull
 
-cd ~/storage/synch; git add . && git commit -m "$(date)"; git push
+cd ~/storage/shared/synch; git add . && git commit -m "$(date)"; git push
